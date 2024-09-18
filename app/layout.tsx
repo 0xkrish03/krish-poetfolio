a73fb816da19e2fbf615ae43f8ecac847a7c8aaf
0xkrish03 <166608367+0xkrish03@users.noreply.github.com>
2024-09-19 01:01:18 +0530

    added a file to store a photo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "@/components/Header";
-import PageTransition from "@/components/PageTransition";
-import StairTransition from "@/components/StairTransition";
+import Header from "@/components/navbar/Header";
+import PageTransition from "@/components/animation/PageTransition";
+import StairTransition from "@/components/animation/StairTransition";
 
 export const metadata: Metadata = {
   title: "KR-Portfolio",
