ceeefb35c61f4a373de6493dc130da980dd795b3
0xkrish03 <166608367+0xkrish03@users.noreply.github.com>
2024-09-19 20:46:25 +0530

    completed the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Button from "@/components/Button";
+import Photo from "@/components/animation/Photo";
 import Socials from "@/components/Socials";
 import { FiDownload } from "react-icons/fi";
+import Stats from "@/components/Stats";
 
 export default function Home() {
   return (
@@ -8,7 +10,7 @@ export default function Home() {
       <div className="container mx-auto h-full">
         <div className="flex flex-row items-center justify-between pt-8 pb-24">
           {/* text */}
-          <div className="text-left">
+          <div className="text-left ">
             <span className="text-xl">Frontend Developer</span>
             <h1 className="h1 mb-6">
               Hello I'm <br />{" "}
@@ -28,16 +30,19 @@ export default function Home() {
               <div>
                 <Socials
                   containerStyles="flex gap-6"
-                  iconStyles="w-9 h-9 border boreder-green-500 rounded-full flex justify-center items-center text-green-500 text-base hover:bg-green-500 
+                  iconStyles="w-9 h-9 border border-green-500 rounded-full flex justify-center items-center text-green-500 text-base hover:bg-green-500 
                                 hover:text-stone-700 hover:transition-all duration-500 "
                 />
               </div>
             </div>
           </div>
           {/* photo */}
-          <div>photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 }
